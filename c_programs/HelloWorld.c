08821b8693b86119b0be2923dba5f43c9b7a5b1d
Chris Villarreal <cjvillar@users.noreply.github.com>
2022-12-24 13:29:26 -0800

    updating hello

--- c_programs/HelloWorld.c
+++ c_programs/HelloWorld.c
@@ -1,6 +1,18 @@
+
+/*
+The first line of the program #include <stdio.h> is a preprocessor command, which tells a C compiler to include stdio.h file before going to actual compilation.
+
+The next line int main() is the main function where the program execution begins.
+
+The next line printf(...) is another function available in C which causes the message "Hello, World!" to be displayed on the screen.
+
+The next line return 0; terminates the main() function and returns the value 0.
+
+*/
+
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Hello, World! \n");
+
    return 0;
 }
