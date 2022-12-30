b64efeed23784d4c1d4c79697d4db386b3338cd6
Chris Villarreal <cjvillar@users.noreply.github.com>
2022-12-29 21:25:17 -0800

    updating for loop temp convert

--- c_programs/ForLoopTempConvert.c
+++ c_programs/ForLoopTempConvert.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+/* Here we will use define name value
+so we don't have random numbers in our code that will confuse future 
+you.
+
+*/
+
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit of table */
+#define STEP 20 /* step size */
+
 main(){
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n ", fahr, (5.0/9.0) * (fahr-32));
 
 }
